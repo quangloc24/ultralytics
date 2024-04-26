7415f66e0cc476801a4f9be7ad0d46c6e2ab6193
quanglocle24 <101305749+quangloc24@users.noreply.github.com>
2024-04-26 20:35:11 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -62,4 +62,4 @@ def process_image():
         return jsonify({'error': 'Unsupported file format'}), 415
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=3000, debug=True)
